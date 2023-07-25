00e8c5a170066ee56c1c6a46f3e085bfc4a6d39f
Dang Huy Nguyen <danghuy.kstnbk@gmail.com>
2023-07-25 16:31:02 +0700

    Add solution for 'Movie Festival'

---
+++ 1629.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import math
+import os
+import sys
+from bisect import bisect_left, bisect_right
+from io import BytesIO, IOBase
+
+
+def solve():
+    """Write code here"""
+    N = int(input())
+    output = 0
+    intervals = []
+    for _ in range(N):
+        a, b = map(int, input().split())
+        intervals.append((a, b))
+    intervals.sort(key=lambda x: x[1])
+    last_end = 0
+    for start, end in intervals:
+        if start >= last_end:
+            output += 1
+            last_end = end
+    return output
+
+
+def main():
+    output = solve()
+    if isinstance(output, bool):
+        if output:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print(output)
+
+
+# region fastio
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+    main()
+
